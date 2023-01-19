e9ea09a2f6c93397ba7ccfbde98b5ac9b19cdb3f
cyrenevjacob <cyrenevjacob@hotmail.com>
2023-01-19 12:32:19 +0000

    updated mountain vue

--- pages/_continent/_mountain.vue
+++ pages/_continent/_mountain.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-      <h1>Continent: {{ continent }}</h1>
+      <div style="text-align: center; margin: 0; padding: 1em;"><h1 style="display: inline-block; font-weight: 200;">Continent:</h1>&nbsp;<h1 style="color:red;display: inline-block; font-weight: bold;">{{ continent }}</h1></div>
       <h2>Mountain: {{ mountain }}</h2>
-      <p>Path...: {{ $route.path }}</p>
+      <p>Path: {{ $route.path }}</p>
       <NuxtLink to="/">Back to Mountains</NuxtLink>
     </div>
   </template>
