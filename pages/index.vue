5c1573c3f6918ceac5a9df0fcc86e239105b4e2b
cyrenevjacob <cyrenevjacob@hotmail.com>
2023-01-19 10:48:39 +0000

    first push

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
+        <li>{{ mountain.title }}</li>
+      </NuxtLink>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+
+    return { mountains }
+  }
+}
+</script>
+
