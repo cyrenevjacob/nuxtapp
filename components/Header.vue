ec4073e3c22b32bf065d41b07142608aad97b603
cyrenevjacob <cyrenevjacob@hotmail.com>
2023-03-22 16:59:41 +0000

    -no traffic, updated header, title font style

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
     <div style="background-color:#e5e5e5;padding:15px;text-align:center;">
-        <h1>Hello World</h1>
+        <h1 class="headerTitle">Mountain Due</h1>
     </div>
 </template>
